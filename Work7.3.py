c409e88022d2cbf1aeee1970ccb903d54381a6ac
Myluckunder0 <inthira_ta@hotmail.com>
2024-02-24 23:30:57 +0700

    Create Work7.3.py

---
+++ Work7.3.py
@@ -0,0 +1,8 @@
+cusername = "admin"
+cpassword = "Ad13n@23t"
+username = input("Username: ")
+password = input("Password: ")
+if username == cusername and password == cpassword:
+    print("เข้าสู่ระบบสำเร็จ")
+else:
+    print("เข้าสู่ระบบไม่สำเร็จ")
